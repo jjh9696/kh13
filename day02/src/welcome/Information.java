b1eb84c2ea86dc0fc096ca3df61a8f9cf52c17b5
jjh9696 <cnqzmf@naver.com>
2023-12-05 12:53:15 +0900

    줄바꿈

--- day02/src/welcome/Information.java
+++ day02/src/welcome/Information.java
@@ -4,7 +4,8 @@ public class Information {
 
 	public static void main(String[] args) {
 
-		System.out.println("정지환, (디지털컨버전스)공공데이터 융합 웹 애플리케이션 개발자 양성과정7");
+		System.out.println("정지환");
+		System.out.println("(디지털컨버전스)공공데이터 융합 웹 애플리케이션 개발자 양성과정7");
 	}
 
 }

e817fb0edc14b26a77cd9e3f43bc2b3134019370
jjh9696 <cnqzmf@naver.com>
2023-12-06 10:50:30 +0900

    나눗셈 파일 생성

---
+++ day03/src/data/Test08나눗셈.java
@@ -0,0 +1,13 @@
+package data;
+
+public class Test08나눗셈 {
+	
+	public void main (String[] args) {
+		
+		int number = 75;
+		//위 숫자의 10의자리, 1의 자리를 각각 출력
+		System.out.println(number/10);
+		System.out.println(number%10);
+		
+	}
+}

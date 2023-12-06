9da85931f9b8ee01db393873df846273e17f0287
jjh9696 <cnqzmf@naver.com>
2023-12-06 18:13:44 +0900

    문자, 문자열 설명과 페이지번호계산기 문제

---
+++ day03/src/data3/Test01문자.java
@@ -0,0 +1,24 @@
+package data3;
+
+public class Test01문자 {
+
+	public static void main(String[] args) {
+
+		/*	자바의 문자 표현방식
+			-자바는 유니코드셋을 표현할 수 있다
+			-char 형태로 표현 (한 글자만 가능)
+		
+		
+		*/
+		char a = '힣';
+		System.out.println(a);
+		
+		int b = a;
+		System.out.println(b);
+		
+		char c = 44032;
+		System.out.println(c);
+		
+	}
+
+}

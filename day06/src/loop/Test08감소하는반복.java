ca9e347c82a2a63cafecbd210a3a84892c735db7
jjh9696 <cnqzmf@naver.com>
2023-12-11 17:59:38 +0900

    Test08설명추가 Test09타이머문제, 기능추가

--- day06/src/loop/Test08감소하는반복.java
+++ day06/src/loop/Test08감소하는반복.java
@@ -5,6 +5,9 @@ public class Test08감소하는반복 {
 	public static void main(String[] args) {
 
 		for(int i=10;i>=0;i--) {
+//			Q) 10부터 0까지 감소하는 카운트다운
+//			범위조심
+// 			증가가 아닌 감소 처리를 해야함
 			System.out.println(i);
 			
 		}
---
+++ day06/src/loop/Test09타이머1초멈추기추가.java
@@ -0,0 +1,32 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Test09타이머1초멈추기추가 {
+
+	public static void main(String[] args) {
+		
+/*		사용자에게 '분'과 '초'를 입력받아서 해당하는 시간만큼의 메세지를 출력하는 프로그램을 구현
+		분 입력 : 1
+		초 입력 : 30
+		
+		1분 30초 후 알람이 울립니다
+		1분 29초 후 알람이 울립니다
+		1분 28초 후 알람이 울립니다
+		...
+		0분 2초 후 알람이 울립니다
+		0분 1초 후 알람이 울립니다
+		0분 0초 후 알람이 울립니다
+		띠링!
+*/		
+		Scanner a = new Scanner(System.in);
+		System.out.print("분을 입력하세요 : ");
+		int min = a.nextInt();
+		System.out.print("초를 입력하세요 : ");
+		int sec = a.nextInt();
+		for (int time = min*60+sec;time>=0;time--) {
+			System.out.println(time/60+"분 "+time%60+"초 후 알람");
+		}
+		System.out.println("띠링");
+	}
+}

8be0e04e052bd0791b834470be7b2646c3b7e24c
jjh9696 <cnqzmf@naver.com>
2023-12-11 14:16:04 +0900

    Test01,Test02,Test03

--- day06/src/loop/Test01반복문.java
+++ day06/src/loop/Test01반복문.java
@@ -2,8 +2,7 @@ package loop;
 
 public class Test01반복문 {
 
-	public static void main(String[] args) {
-		//반복을 위해서는 변수가 하나 필요하다 (반복수, 반복변수
+	public static void main(String[] args) { 
 		//반복수를 변화시켜 원하는 만큼의 횟수를 만든다
 	for(int i=1; i<=5 ;i=i+1) {
 		System.out.println("Hello Java");
---
+++ day06/src/loop/Test03반복변수활용.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class Test03반복변수활용 {
+
+	public static void main(String[] args) { 
+		
+		for(int i=1; i<=10 ;i++) {
+			//for 내에서는 반복에 사용한 변수 i를 쓸 수 있다
+			System.out.println("Hello Java"+i);
+		}
+		
+	}
+
+}

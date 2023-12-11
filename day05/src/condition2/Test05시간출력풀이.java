d64b69cd6590d1eb1cf9e2eee2d6611b7569f223
jjh9696 <cnqzmf@naver.com>
2023-12-11 10:08:19 +0900

    Test05시간출력 풀이

--- day05/src/condition2/Test05시간출력풀이.java
+++ day05/src/condition2/Test05시간출력풀이.java
@@ -2,7 +2,7 @@ package condition2;
 
 import java.util.Scanner;
 
-public class Test05시간출력 {
+public class Test05시간출력풀이 {
 
 	public static void main(String[] args) {
 /*		SNS에서는 작성한 글이 몇 초 전에 작성되었는지에 따라 다음과 같이 출력하도록 구성되어 있습니다.
@@ -15,32 +15,31 @@ public class Test05시간출력 {
 		?일 전 - 작성한 지 1일 이상인 글
  * 
  */
+		//입력
 		Scanner sc = new Scanner(System.in);
 		System.out.print("초를 입력하세요 : ");
 		int writeTime = sc.nextInt();
 		
-		boolean before = writeTime < 10;
-		boolean sec = writeTime < 60; // && writeTime>=10 	//의미없는 계산 제거
-		boolean	min =  writeTime < 60*60; // && writeTime>=60 
-		boolean hour = writeTime < 60*60*24; // && writeTime>=60*60
-		
 		String status;
 		
-		if(before) {
+		//계산
+		if(writeTime < 10) {
 			status = "방금 전";
 		}
-		else if(sec) {
+		else if(writeTime < 60) {// && writeTime>=10	//의미없는 계산 제거
 			status = writeTime+"초 전";
 		}
-		else if(min) {
+		else if(writeTime < 60*60) {// && writeTime>=60	//의미없는 계산 제거
 			status = writeTime/60+"분 전";
 		}
-		else if(hour) {
+		else if(writeTime < 60*60*24) {// && writeTime>=60*60	//의미없는 계산 제거
 			status = writeTime/60/60+"시간 전";
 		}
 		else {
 			status = writeTime/60/60/24+"일 전";
 		}
+		
+		//출력
 		System.out.println(status+"에 작성");
 	}
 

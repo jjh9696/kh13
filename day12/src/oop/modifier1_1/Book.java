1ab26edc26722660b07d2d14959e8eae0202e5d9
jjh9696 <cnqzmf@naver.com>
2023-12-19 17:56:30 +0900

    생성자 예제

--- day12/src/oop/modifier1_1/Book.java
+++ day12/src/oop/modifier1_1/Book.java
@@ -12,7 +12,6 @@ public class Book {
 	//변수가 잠기면 외부에서는 직접 접근할 수 있는 방법이 없다
 	//-설정을 위한 setter 메소드, 확인을 위한 getter 메소드를 설정해야 한다
 	
-	
 	public String getTitle() {
 		return title;
 	}
--- day12/src/oop/modifier3/Phone.java
+++ day12/src/oop/modifier3/Phone.java
@@ -90,5 +90,4 @@ public class Phone {
 		System.out.println();
 		
 	}
-	
 }

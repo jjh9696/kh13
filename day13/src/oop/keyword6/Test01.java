96cbd597a1d9376ba806f3a0328392c7b7e80d2d
jjh9696 <cnqzmf@naver.com>
2023-12-20 16:51:39 +0900

    final 활용문제 게임스크린

---
+++ day13/src/oop/keyword6/Test01.java
@@ -0,0 +1,21 @@
+package oop.keyword6;
+
+import java.util.Scanner;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("폭, 높이, 제목을 입력하세요");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		String c = sc.next();
+	
+		GameScreen a1 = new GameScreen(a, b, c);
+		a1.information();
+//		System.out.println(GameScreen.resolution(a, b));
+		
+	}
+
+}

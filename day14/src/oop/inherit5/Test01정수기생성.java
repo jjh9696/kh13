a3c771d1908bcd350eb1d6922f39bdad539c207c
jjh9696 <cnqzmf@naver.com>
2023-12-21 14:58:53 +0900

    상위클래스

--- day14/src/oop/inherit5/Test01정수기생성.java
+++ day14/src/oop/inherit5/Test01정수기생성.java
@@ -7,6 +7,7 @@ public class Test01정수기생성 {
 		IcePurifier a = new IcePurifier("코웨이",500000);
 		MultiPurifier b = new MultiPurifier("SK매직",650000);
 		HotPurifier c = new HotPurifier("쿠쿠", 450000);
+//		Purifier d = new Purifier("샘숭",300000);//상위클래스 생성자를 protected로 만들었기 때문에 패키지가 달라지면 생성x  
 		
 		a.information();
 		a.normal();

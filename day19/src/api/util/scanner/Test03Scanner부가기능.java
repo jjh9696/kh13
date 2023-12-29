6893410965f94609fd6d62d29075ffa122b84bb9
jjh9696 <cnqzmf@naver.com>
2023-12-29 17:28:57 +0900

    Scanner 부가기능

---
+++ day19/src/api/util/scanner/Test03Scanner부가기능.java
@@ -0,0 +1,20 @@
+package api.util.scanner;
+
+import java.util.Scanner;
+
+public class Test03Scanner부가기능 {
+
+	public static void main(String[] args) {
+
+		String colors = "빨강+주황+노랑+초록+파랑+남색+보라";
+		
+		Scanner sc = new Scanner(colors);
+//		sc.useDelimiter("[+]");// +가 구분자임을 알려준다
+		sc.useDelimiter("[^가-힣$]");
+		while(sc.hasNext()) {
+			System.out.println(sc.next());
+		}
+		sc.close();
+	}
+
+}

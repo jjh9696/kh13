b90c49e062f96017616d8e44799aea6ba07af9bd
jjh9696 <152838260+jjh9696@users.noreply.github.com>
2024-01-04 14:25:26 +0900

    Create 01.테이블생성.sql

---
+++ database/01.테이블생성.sql
@@ -0,0 +1,25 @@
+create table soccer(
+rank number(2), 
+name varchar2(21), 
+win number(2), 
+draw number(2), 
+lose number(2));
+DROP TABLE soccer;
+
+create table Savings( 
+name varchar2(21), 
+basic number(5,2),
+prefer number(5,2),
+monthly number, 
+year number(3));
+drop table savings
+
+create table athlete(
+name varchar2(21), 
+event varchar2(11), 
+season char(6), 
+gold number(2), 
+silver number(2), 
+bronze number(2));
+DROP TABLE ATHLETE;
+

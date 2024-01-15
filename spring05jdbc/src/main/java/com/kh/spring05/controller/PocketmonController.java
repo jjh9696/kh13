3a7ddd293cb4ae392d5c78d562e41d29d7397c2a
jjh9696 <cnqzmf@naver.com>
2024-01-15 15:32:13 +0900

    포켓몬 dao dto mapper template 전부 다 참고

--- spring05jdbc/src/main/java/com/kh/spring05/controller/PocketmonController.java
+++ spring05jdbc/src/main/java/com/kh/spring05/controller/PocketmonController.java
@@ -2,16 +2,19 @@ package com.kh.spring05.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kh.spring05.dao.PocketmonDao;
 import com.kh.spring05.dto.PocketmonDto;
 
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @RestController
+@RequestMapping("/pocketmon")//공용주소 http://localhost:8080/pocketmon/insert~
 public class PocketmonController {
 
 	//(중요) 등록된 것들은 "주세요~"가 된다 (DI, 의존성 주입)
@@ -45,7 +48,7 @@ public class PocketmonController {
 	//@RequestParam은 한개씩 저장할때 사용
 	@RequestMapping("/insert2")
 	public String insert2(PocketmonDto dto) {
-	//public String insert2(@ModelAttribute (PocketmonDto dto){
+	//public String insert2(@ModelAttribute (PocketmonDto dto){ //원래는 이렇게 써야하는데 일단은 객체를 넣는 느낌을 익히자 
 		String sql = "insert into pocketmon("
 				+ "pocketmon_no, "
 				+ "pocketmon_name, "
@@ -56,5 +59,18 @@ public class PocketmonController {
 		
 		return "포켓몬스터 등록 완료";
 	}
+	
+	//업그레이드
+	//-DB처리를 DAO에게 전담하여 처리
+	
+	@Autowired
+	private PocketmonDao dao;
+	
+	@RequestMapping("/insert3")
+	public String insert3 (@ModelAttribute PocketmonDto dto) {
+		dao.insert(dto);
+		return "포켓몬스터 등록 완료";
+	}
+	
 	
 }
---
+++ spring05jdbc/src/main/java/com/kh/spring05/dao/PocketmonDao.java
@@ -0,0 +1,34 @@
+package com.kh.spring05.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring05.dto.PocketmonDto;
+import com.kh.spring05.mapper.PocketmonMapper;
+
+//DAO도 이제부터는 등록을 해야한다
+//-@Repository는 파일이나 데이터베이스와 같이 영속성(영원함) 개체를 제어하는 도구
+@Repository
+public class PocketmonDao {
+	
+	@Autowired //이걸 누락시켜도 에러가 안나지만 실행하면 에러가 남
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired	
+	private PocketmonMapper mapper;
+	
+	public void insert(PocketmonDto dto) {
+		String sql = "insert into pocketmon("
+				+"pocketmon_no, "
+				+ "pocketmon_name, "
+				+ "pocketmon_type"
+				+ ") values (?,?,?)";
+		Object[] data = {
+				dto.getPocketmonNo(),dto.getPocketmonName(),dto.getPocketmonType()
+		};
+		jdbcTemplate.update(sql,data);
+		
+	}
+	
+}

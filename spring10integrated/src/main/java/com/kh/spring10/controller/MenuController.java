02df8699548dec1c751c0d77cab09ee5d6e6d8ee
jjh9696 <cnqzmf@naver.com>
2024-01-18 15:03:59 +0900

    menu 페이지

---
+++ spring10integrated/src/main/java/com/kh/spring10/controller/MenuController.java
@@ -0,0 +1,38 @@
+package com.kh.spring10.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.spring10.dao.Menudao;
+import com.kh.spring10.dto.MenuDto;
+
+
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+	
+	@Autowired
+	private Menudao dao;
+
+	@GetMapping("/insert")
+	public String insert() {
+		return "/WEB-INF/views/menu/insert.jsp";
+	}
+	
+	@PostMapping("/insert")
+	public String insert(@ModelAttribute MenuDto dto) {
+		dao.insert(dto);
+		return "redirect:insertComplete";
+	}
+	
+	@RequestMapping("/insertComplete")
+	public String insertComplete() {
+		return "/WEB-INF/views/menu/insertComplete.jsp";
+	}
+	
+	
+}
--- spring10integrated/src/main/webapp/WEB-INF/views/menu/insertComplete.jsp
+++ spring10integrated/src/main/webapp/WEB-INF/views/menu/insertComplete.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-    
-    <h1>직원 등록 완료!</h1>
-    
+
+    <h1>메뉴 등록 완료!</h1>
+    <h2><a href="insert">다시 등록하기</a></h2>

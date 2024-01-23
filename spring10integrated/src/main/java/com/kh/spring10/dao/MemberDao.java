0dac6ac13f5f5edc139fb48072db674e1665bce3
jjh9696 <cnqzmf@naver.com>
2024-01-23 15:42:54 +0900

    모든 데이터 저장

---
+++ spring10integrated/src/main/java/com/kh/spring10/dao/MemberDao.java
@@ -0,0 +1,93 @@
+package com.kh.spring10.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring10.dto.MemberDto;
+import com.kh.spring10.mapper.MemberMapper;
+
+
+//member 테이블 데이터 처리를 담당하는 클래스
+@Repository
+public class MemberDao {
+	//메소드에서 공용으로 사용하는 도구들을 생성
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private MemberMapper mapper = new MemberMapper();
+	
+	//가입(등록, Create) Test01
+	public void insert(MemberDto dto) {
+		String sql = "insert into member("
+							+ "member_id, member_pw, member_nick, "
+							+ "member_birth, member_contact, member_email, "
+							+ "member_post, member_address1, member_address2"
+						+ ") values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+//		String sql = "insert into member("
+//							+ "member_id, member_pw, member_nick, "
+//							+ "member_birth,member_contact, member_email, "
+//							+ "member_post, member_address1, member_address2,"
+//							+ "member_level, member_point, member_join, member_login"
+//						+ ") values(?, ?, ?, ?, ?, ?, ?, ?, ?, '일반회원', 0, sysdate, null)";	//디폴트 값을 쓰는게 아닌 내가 넣어주는 경우
+		Object[] data = {
+			dto.getMemberId(), dto.getMemberPw(), dto.getMemberNick(),
+			dto.getMemberBirth(), dto.getMemberContact(), dto.getMemberEmail(),
+			dto.getMemberPost(), dto.getMemberAddress1(), dto.getMemberAddress2()
+		};
+		jdbcTemplate.update(sql, data);
+	}
+
+	//수정 (Update)
+	public boolean update(MemberDto dto) {
+		String sql = "update member set "
+				+ "member_id=?, member_pw=?, member_nick=?, member_birth=?, member_contact=?, member_email=?, member_post=?, member_address1=?, member_address2=?";
+		Object[] data = {
+				dto.getMemberId(), dto.getMemberPw(), dto.getMemberNick(), dto.getMemberBirth(), dto.getMemberContact(), dto.getMemberEmail(), dto.getMemberPost(), dto.getMemberAddress1(), dto.getMemberAddress2()
+		};
+		return jdbcTemplate.update(sql, data)>0;
+	}
+	
+	//비밀번호 변경(수정, Update) Test04
+	public boolean updateMemberPw(MemberDto dto) {
+		String sql = "update member set "
+				+ "member_pw=? where member_id=?";
+		Object[] data = {
+				dto.getMemberPw(), dto.getMemberId()
+		};
+		return jdbcTemplate.update(sql, data)>0;
+	}
+	
+	//삭제 Test05
+	public boolean delete (String memberId) {
+		String sql = "delete member where member_id=?";
+		Object[] data = {memberId};
+		return jdbcTemplate.update(sql,data)>0;
+	}
+	
+	//목록(조회, Read) Test02,06
+	public List<MemberDto> selectList(){
+		String sql = "select * from member order by member_id asc";
+//		Object[] data = {}
+		return jdbcTemplate.query(sql, mapper);
+	}
+	
+	//상세(조회, Read) Test01,03,04,05,06
+	public MemberDto selectOne(String memberId) {
+		String sql = "select * from member where member_id=?";
+		Object[] data = {memberId};
+		List<MemberDto> list = jdbcTemplate.query(sql, mapper, data);
+		return list.isEmpty() ? null : list.get(0);
+	}
+	
+	//검색(조회, Read)
+	public List<MemberDto> selectList(String column, String keyword) {
+		String sql = "select * from member where instr("+column+", ?) > 0 "
+										+ "order by "+column+" asc, member_id asc";
+		Object[] data = {keyword};
+		return jdbcTemplate.query(sql, mapper, data);
+	}
+	
+}

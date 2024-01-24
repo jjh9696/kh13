3f9386a42128d32887503a9ffc512aa4e20fb71f
jjh9696 <cnqzmf@naver.com>
2024-01-24 12:57:49 +0900

    로그인 로그인실패

--- spring10integrated/src/main/java/com/kh/spring10/controller/MemberController.java
+++ spring10integrated/src/main/java/com/kh/spring10/controller/MemberController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.kh.spring10.dao.MemberDao;
 import com.kh.spring10.dto.MemberDto;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.Session;
+
 
 @Controller
 @RequestMapping("/member")
@@ -19,6 +22,7 @@ public class MemberController {
 	@Autowired
 	private MemberDao memberDao;
 	
+	//회원가입
 	@GetMapping("/join")
 	public String join() {
 		return "/WEB-INF/views/member/join.jsp";
@@ -35,14 +39,62 @@ public class MemberController {
 		return "/WEB-INF/views/member/joinFinish.jsp";
 	}
 	
+	//실제 로그인
+	//-아이디와 비밀번호 검사를 통과해야만 세션에 데이터를 추가한다
+	//-사용자가 입력한 아이디를 추가한다
 	@GetMapping("/login")
 	public String login() {
 		return "/WEB-INF/views/member/login.jsp";
 	}
 	
 	@PostMapping("/login")
-	public String login(@RequestParam String memberId,
-						@RequestParam String memberPw) {
+	public String login(@ModelAttribute MemberDto inputDto, HttpSession session) {
+		//사용자가 입력한 아이디로 회원정보를 조회한다
+		MemberDto findDto = memberDao.selectOne(inputDto.getMemberId());
+		//로그인 가능 여부를 판정
+		boolean isValid = findDto !=null 
+				&& inputDto.getMemberPw().equals(findDto.getMemberPw());
+				//findDto !=null(아이디가 있으면)
+				//inputDto.getMemberPw()(사용자가 입력한 비밀번호)
+				//findDto.getMemberPw()(DB에 저장된 비밀번호)
+		//결과에 따라 다른 처리
+		if(isValid) {
+			//세션에 데이터 추가
+			session.setAttribute("loginId",findDto.getMemberId());
+			return "redirect:/";
+		}
+		else {
+			return "redirect:login?error";//로그인 실패
+		}
+		
+	}
+	
+	//실제 로그아웃
+	//-로그인 때 검사를 했으므로 추가 검사는 불필요
+	//-로그인 때 저장한 세션의 데이터만 삭제 처리
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("loginId");
 		return "redirect:/";
 	}
+	
+	//테스트 로그인 & 로그아웃
+	//-HttpSession을 사용하고 싶다면 매개변수에 선언만 하면 된다
+	//-등록 : session.setAttribute("key", value)
+	//-확인 : session.getAttribute("key")
+	//-삭제 : session.removeAttribute("key")
+	@RequestMapping("/testLogin")
+	public String testLogin(HttpSession session) {
+		//아이디만 있으면 모든 정보를 불러올 수 있으므로 아이디를 저장
+		session.setAttribute("loginId", "testuser1");
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/testLogout")
+	public String testLogout(HttpSession session) {
+		session.removeAttribute("loginId");
+		return "redirect:/";	
+	}
+	
+	
 }
--- spring10integrated/src/main/webapp/WEB-INF/views/member/login.jsp
+++ spring10integrated/src/main/webapp/WEB-INF/views/member/login.jsp
@@ -1,9 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>		
+			
 <jsp:include page="/WEB-INF/views/template/header.jsp"></jsp:include>
 
+	<h1>로그인</h1>
+	
+	<c:if test="${param.error != null}">
+		<h3 style="color:red"; style="backgroun-color:blue";>로그인 정보가 일치하지 않습니다</h3>
+	</c:if>
+	
     <form action="login" method="post">
     	아이디  <input type="text" name="memberId" required> <br><br>
 		비밀번호  <input type="password" name="memberPw" required><br><br>
@@ -11,3 +17,4 @@
 	</form>
 
 <jsp:include page="/WEB-INF/views/template/footer.jsp"></jsp:include>
+

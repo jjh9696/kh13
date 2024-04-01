3feea2f4891feef3cb57b894c5f641ebcd68bcac
jjh9696 <cnqzmf@naver.com>
2024-04-01 17:17:55 +0900

    top n query 적용

--- spring15mybatis/src/test/java/com/kh/spring15/ComplexSearchTest04.java
+++ spring15mybatis/src/test/java/com/kh/spring15/ComplexSearchTest04.java
@@ -28,8 +28,24 @@ public class ComplexSearchTest04 {
 		//column과 keyword 유무에 따라 mybatis 구문이 변화하여 실행된다
 		//즉, 자바에서는 조건으로 검색인지 목록인지 구분할 필요가 없다(DB에 관해서)
 		Map<String, Object> data = new HashMap<>();
-		data.put("memberId", "test");
-		data.put("memberNick", "테스트");
+//		data.put("memberId", "test");
+//		data.put("memberNick", "테스트");
+//		data.put("memberContact", "01012345678");
+//		data.put("minPoint", "10000");
+//		data.put("maxPoint", "30000");
+//		data.put("memberJoin", "2024-01-31");
+//		data.put("minLoginDate", "2024-03-01");
+//		data.put("maxLoginDate", "2024-03-15");
+//		data.put("memberLevelList", List.of()); //값이 없으니 구문이 작동안됨 select * from member가 작동 됨
+//		data.put("memberLevelList", List.of("일반회원"));
+//		data.put("memberLevelList", List.of("일반회원", "우수회원"));
+//		data.put("memberLevelList", List.of("일반회원", "우수회원", "관리자"));
+//		data.put("orderList", List.of("member_nick asc", "member_point desc"));
+		
+		data.put("beginRow", 1);
+		data.put("endRow", 10);
+		
+		
 		
 		List<MemberDto> list = sqlSession.selectList("member.complex", data);
 		for(MemberDto memberDto : list) {

d5949bd1ec97c1ddfc82005f3d7785b33508e5a9
jjh9696 <cnqzmf@naver.com>
2024-04-19 16:11:54 +0900

    에러 restController

--- spring10integrated/src/main/java/com/kh/spring10/error/ErrorController.java
+++ spring10integrated/src/main/java/com/kh/spring10/error/ErrorController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 //이미 만들어진 컨트롤러에서 발생하는 예외에 대한 처리를 수행하는 도구
-//-간섭 도구이면 AOP의 구현체 중 하나
+//-간섭 도구이며 AOP의 구현체 중 하나
 //-등록을 @ControllerAdvice로 하며, 옵션으로 대상을 반드시 지정해야 한다
 //-클래스 내에는 @ExceptionHandler를 만들어 예외를 종류별로 처리
 //-메소드 내에서 필요한 처리를 하고 보여주고 싶은 화면을 반환

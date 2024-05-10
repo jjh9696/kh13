c08cd15adaa59112d2b2d4660e8f1e0a0ab6980d
jjh9696 <cnqzmf@naver.com>
2024-05-10 18:50:01 +0900

    멤버 목록 불러오기 회원이 볼 때 관리자가 보낸 채팅은 상담사가 보냈다고 표시 채팅모달

--- springtest01/src/main/java/com/kh/springtest01/vo/JustMemberInfoVO.java
+++ springtest01/src/main/java/com/kh/springtest01/vo/JustMemberInfoVO.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 //채팅 목록에 회원 아이디만 넘겨주기 위한 VO
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
-public class JustMemberIdVO {
+public class JustMemberInfoVO {
 	private String memberId;//회원아이디
-}
+	private String memberLevel;//회원아이디
+}	
